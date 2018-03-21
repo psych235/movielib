7935f56b87310e5715fc5d79f300a308eb995629
Mike Bongorno <mbongorno@gmail.com>
2018-03-21 18:46:24 -0400

    Remove nested attribute access

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,15 +9,6 @@ class Movie < ApplicationRecord
   has_many :directors, through: :directors_movies
   has_many :ratings, through: :ratings_movies
   has_many :actors, through: :actors_movies
-  accepts_nested_attributes_for :directors_movies
-  accepts_nested_attributes_for :actors
-
-  def directors_attributes=(director_attributes)
-    director_attributes.values.each do |director_attribute|
-      director = Director.find_or_create_by(director_attribute)
-      self.directors << director
-    end
-  end
 
   def list_directors
     list = []
